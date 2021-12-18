8313f26fd42cae1b888d6364c825ec2980045d37
Pere <perepicomuntaner@gmail.com>
2021-12-18 08:56:21 +0100

    Typescript project boilerplate

---
+++ __tests__/index.spec.ts
@@ -0,0 +1,8 @@
+import assert from "assert";
+import { describe } from "mocha";
+
+describe("Tests named spec.ts", function () {
+  it("should run and pass", () => {
+    assert.equal(1, 1);
+  });
+});
---
+++ __tests__/index.test.ts
@@ -0,0 +1,8 @@
+import assert from "assert";
+import { describe } from "mocha";
+
+describe("Tests named test.ts", function () {
+  it("should run and pass", () => {
+    assert.equal(1, 1);
+  });
+});
